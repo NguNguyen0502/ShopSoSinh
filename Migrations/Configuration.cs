1ad8c96c98559ea6174990972d050e9eaa1bb4e9
NguNguyen0502 <37398746+NguNguyen0502@users.noreply.github.com>
2018-07-10 22:13:32 +0700

    Add files via upload

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ShopBeSoSinh.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ShopBeSoSinh.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ShopBeSoSinh.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ShopBeSoSinh.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
