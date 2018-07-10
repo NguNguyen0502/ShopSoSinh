b7e65e4e0cb6b3876bba6916c83eb0b86aa82a40
NguNguyen0502 <37398746+NguNguyen0502@users.noreply.github.com>
2018-07-10 13:14:55 +0700

    Add files via upload

---
+++ ShopSoSinh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopBeSoSinh.MvcApplication" Language="C#" %>
---
+++ ShopSoSinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopBeSoSinh.Startup))]
+namespace ShopBeSoSinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
