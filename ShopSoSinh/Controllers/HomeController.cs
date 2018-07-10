6d6d10089debe672c7ba8751835949d935c9efe3
NguNguyen0502 <37398746+NguNguyen0502@users.noreply.github.com>
2018-07-10 22:10:09 +0700

    Add files via upload

---
+++ ShopSoSinh/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopBeSoSinh.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        ShopBeSoSinhDbContext db = new ShopBeSoSinhDbContext();
+        public ActionResult Index()
+        {
+            var model = db.Categories
+                .Where(c => c.Products.Count >= 4)
+                .OrderBy(c => Guid.NewGuid()).ToList();
+                
+           
+            return View(model);
+        }
+
+        public ActionResult Search()
+        {
+            var name = Request["term"];
+
+            var data = db.Products
+                .Where(p => p.Name.Contains(name))
+                .Select(p => p.Name).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        public ActionResult Category()
+        {
+            var model = db.Categories;
+            return PartialView("_Category",model);
+        }
+    }
+}
